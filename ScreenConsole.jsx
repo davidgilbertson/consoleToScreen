7afae6ecc97f34825448fe281ea0145023299083
David Gilbertson <DGilbertson@bauer-media.com.au>
2015-03-10 13:31:07 +1100

    Added React.js component version.

---
+++ ScreenConsole.jsx
@@ -0,0 +1,115 @@
+/**
+ * @jsx React.DOM
+ */
+
+'use strict';
+
+var React = require('react/addons'),
+    _ = require('underscore'),
+    options = {
+        show: 'always',
+        windowErrors: true,
+        pos: 'bottom'
+    },
+    consoleShowing = false,
+    consoleEl,
+    logBody,
+    line = 1,
+    style = {
+        'position': 'fixed',
+        'display': 'none',
+        'left': '0',
+        'right': '0',
+        'bottom': '-2',
+        'height': '20%',
+        'padding': '10px',
+        'background': '#111',
+        'backfaceVisibility': 'hidden',
+        'WebkitOverflowScrolling': 'touch',
+        'fontFamily': 'monospace',
+        'fontSize': '12px',
+        'lineHeight': '14px',
+        'color': '#eee',
+        'overflow': 'auto',
+        'boxShadow': '0px -2px 10px rgba(0, 0, 0, 0.5)',
+        'zIndex': '9999'
+    };
+
+var ScreenConsole = React.createClass({
+
+    componentDidMount: function() {
+        consoleEl = this.getDOMNode();
+        logBody = this.refs.logBody.getDOMNode();
+
+        this.overrideConsoleLog();
+        this.showConsole();
+    },
+
+    renderLine: function(args) {
+        if (!consoleShowing) {
+            this.showConsole();
+        }
+
+        var msg = '', i;
+
+        for (i = 0; i < args.length; i++) {
+            msg += args[i];
+
+            if (i < args.length - 1) {
+                msg += ' ';
+            }
+        }
+
+        logBody.innerHTML += '<pre>' + line + ': ' + msg + '</pre>';
+        line++;
+        consoleEl.scrollTop = 999999;
+    },
+
+    overrideConsoleLog: function() {
+        // if (!_.has(window, 'console')) { return; }
+
+        var originalConsole = console.log;
+        var self = this;
+
+        console.log = function () {
+            self.renderLine(arguments);
+            Function.prototype.apply.call(originalConsole, console, arguments);
+        };
+    },
+
+    showConsole: function() {
+        consoleEl.style.display = 'block';
+        consoleShowing = true;
+    },
+
+    hideConsole: function() {
+        consoleEl.style.display = 'none';
+        consoleShowing = false;
+    },
+
+    setOption: function(prop, val) {
+        options[prop] = val;
+    },
+
+    setOptions: function(opt) {
+        for (var prop in opt) {
+            if (prop.hasOwnProperty) {
+                options[prop] = opt[prop];
+            }
+        }
+    },
+
+    render: function() {
+        /* jshint ignore:start */
+        return (
+            <div style={style}>
+                Console ready...
+                <div ref="logBody"></div>
+            </div>
+        );
+        /* jshint ignore:end */
+    }
+
+});
+
+module.exports = ScreenConsole;
